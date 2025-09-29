26175a8d27e5c5892d509dfa894428cf79259d97
PhamXuanChuan <xuanchuan11125@gmail.com>
2025-09-30 00:49:32 +0700

    Auth: thêm UserModel, Repository, Business, AuthController, Middleware

---
+++ Model/AuthenticateModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Model/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string Token { get; set; }
+    }
+}
