b1f111b1c60cdd6c1176df706c090955e95bb2f7
Le Gia Bach 250605 <legiabach2.0@gmail.com>
2025-11-14 20:50:59 +0700

    hoàn thành fe stockcard

---
+++ Fontend/Shared/js/stockcardfe.js
@@ -0,0 +1,15 @@
+const avatar = document.getElementById("avatar");
+const information = document.getElementById("information");
+//nút mở setting admin
+avatar.addEventListener("click", function () {
+  information.classList.toggle("active"); //toggle sẽ xoá class nếu nó đã có rồi và thêm vào nếu nó chưa có
+});
+
+document.addEventListener("click", function (e) {
+  if (!avatar.contains(e.target) && !information.contains(e.target)) {
+    information.classList.remove("active");
+  }
+});
+
+
+
